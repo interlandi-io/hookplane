170bf6642e4665a32db10dba76cfe4bcd2999252
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-03-31 14:35:16 -0400

    use native ts path resolution in core vitest config

--- src/core/core/vitest.config.mjs
+++ src/core/core/vitest.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig(() => {
     return {
-        plugins: [tsconfigPaths()],
+        resolve: {
+            alias: {
+                '~': path.resolve(__dirname, './src')
+            }
+        },
         test: {
             globals: true, 
             include: ['./test/*.test.ts']
