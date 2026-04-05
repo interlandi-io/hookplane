6d955b994eaa7340eae8e709988041eb094dfd26
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-05 19:55:04 -0400

    transform hookplane to state

--- src/core/hookplane/src/hookplane.test.ts
+++ src/core/hookplane/src/hookplane.test.ts
@@ -1,20 +1,24 @@
 import { hookplane } from './hookplane.js'
 import { stripeProvider } from '@hookplane/stripe'
 
-const hp = await hookplane({
-    providers: {
-        stripe: {
-            provider: await stripeProvider({
-                apiKey: process.env['STRIPE_API_KEY']!,
-            }),
-            endpoint: '/hooks/stripe',
-            events: ['checkout.session.completed'],
-            config: {
-                name: 'my_endpoint',
-                eventPayload: 'snapshot',
+describe('hookplane', () => {
+    it('constructs', async () => {
+        const hp = await hookplane({
+            baseUrl: 'https://localhost:3000',
+            providers: {
+                stripe: {
+                    provider: await stripeProvider({
+                        apiKey: process.env['STRIPE_API_KEY']!,
+                    }),
+                    endpoint: '/hooks/stripe',
+                    events: ['checkout.session.completed'],
+                    config: {
+                        name: 'my_endpoint',
+                        eventPayload: 'snapshot',
+                    },
+                },
             },
-        },
-    },
+        })
+        console.log(hp)
+    })
 })
-
-export default hp
--- src/core/hookplane/src/hookplane.ts
+++ src/core/hookplane/src/hookplane.ts
@@ -1,10 +1,17 @@
-import { EndpointConfigOf, EventTypeOf, ProviderSet } from '@hookplane/core'
-
-export type Hookplane<TProviderSet extends ProviderSet> = {
-    providers: TProviderSet
-}
+import {
+    State,
+    createBaseUrl,
+    EndpointConfigOf,
+    EventTypeOf,
+    ProviderSet,
+    createProviderSet,
+    Provider,
+    createRelativeUrl,
+    createOrphanEndpointHandle,
+} from '@hookplane/core'
 
 type HookplaneParams<TProviderSet extends ProviderSet> = {
+    baseUrl: string
     providers: {
         [K in keyof TProviderSet]: {
             provider: TProviderSet[K]
@@ -17,13 +24,38 @@ type HookplaneParams<TProviderSet extends ProviderSet> = {
 
 export async function hookplane<TProviderSet extends ProviderSet>(
     params: HookplaneParams<TProviderSet>,
-): Promise<Hookplane<TProviderSet>> {
-    const providers = {} as TProviderSet
-    for (const [name, provider] of Object.entries(params.providers)) {
-        providers[name as keyof TProviderSet] = await provider
+): Promise<State<TProviderSet>> {
+    // We throw in this b/c it touches the API boundary
+    const providersUnvalidated: Record<string, Provider> = {}
+    const providerStates = {} as State<TProviderSet>['providerStates']
+
+    for (const [name, v] of Object.entries(params.providers)) {
+        const { provider, endpoint, events, config } =
+            v as HookplaneParams<TProviderSet>['providers'][typeof name]
+        providersUnvalidated[name] = provider
+
+        const relativeUrl = createRelativeUrl(endpoint)._unsafeUnwrap()
+        providerStates[name as keyof typeof providerStates] = new Map([
+            [
+                createOrphanEndpointHandle(), // TODO: this isn't correct because it will create new endpoints for everything
+                {
+                    // there needs to be a matching heuristic here
+                    relativeUrl,
+                    events,
+                    config,
+                },
+            ],
+        ])
     }
 
+    const baseUrl = createBaseUrl(params.baseUrl)._unsafeUnwrap()
+    const providers = createProviderSet(
+        providersUnvalidated,
+    )._unsafeUnwrap() as TProviderSet
+
     return {
+        baseUrl,
         providers,
+        providerStates,
     }
 }
