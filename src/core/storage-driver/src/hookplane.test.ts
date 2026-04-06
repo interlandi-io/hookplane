72e45048369dfa11e304b61401b9815639f4d6d0
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-05 22:11:48 -0400

    scaffold

---
+++ src/core/storage-driver/src/hookplane.test.ts
@@ -0,0 +1,26 @@
+import { hookplane } from './hookplane.js'
+import { stripeProvider } from '@hookplane/stripe'
+
+describe('hookplane', () => {
+    it('constructs', async () => {
+        const state = await hookplane({
+            baseUrl: 'https://localhost:3000',
+            providers: {
+                stripe: {
+                    provider: await stripeProvider({
+                        apiKey: process.env['STRIPE_API_KEY']!,
+                    }),
+                    endpoint: '/hooks/stripe',
+                    events: ['checkout.session.completed'],
+                    config: {
+                        name: 'my_endpoint',
+                        eventPayload: 'snapshot',
+                    },
+                },
+            },
+        })
+        expect(state.baseUrl).toBe('https://localhost:3000')
+        expect(state.providers['stripe']).toBeDefined()
+        expect(state.providerStates['stripe']).toBeDefined()
+    })
+})
