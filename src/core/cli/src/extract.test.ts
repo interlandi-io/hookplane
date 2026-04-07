c0ad839f98c7dc3c762f60f5ea62973cc4a84b36
Michael Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-07 13:48:50 -0400

    feat: Change hookplane() return to StateUnknown (#27)
    * change hookplane return
    
    * change extraction & harden tests

--- src/core/cli/src/extract.test.ts
+++ src/core/cli/src/extract.test.ts
@@ -11,5 +11,10 @@ describe('extract', () => {
         const state = result.value
         expect(state.baseUrl).toBe('https://localhost:3000')
         expect(state.providers['stripe']).toBeDefined()
+        expect(state.providerStates['stripe']).toBeDefined()
+        const arr = [...state.providerStates['stripe']!.values()]
+        expect(arr[0]).toBeDefined()
+        expect(arr[0]!.relativeUrl).toBe('/hooks/stripe')
+        expect(arr[0]!.events[0]).toBe('checkout.session.completed')
     })
 })
--- src/core/hookplane/src/hookplane.test.ts
+++ src/core/hookplane/src/hookplane.test.ts
@@ -22,5 +22,8 @@ describe('hookplane', () => {
         expect(state.baseUrl).toBe('https://localhost:3000')
         expect(state.providers['stripe']).toBeDefined()
         expect(state.providerStates['stripe']).toBeDefined()
+        const arr = [...state.providerStates['stripe'].values()]
+        expect(arr[0]).toBeDefined()
+        expect(arr[0]!.relativeUrl).toBe('/hooks/stripe')
     })
 })
