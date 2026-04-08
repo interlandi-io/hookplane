f327ab7ec528784ac482bc8988475fb516465492
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-08 10:25:20 -0400

    better test

--- src/core/orchestrator/src/index.test.ts
+++ src/core/orchestrator/src/index.test.ts
@@ -1,4 +1,6 @@
 import {
+    BaseUrl,
+    bootstrap,
     defaultDispatch,
     parallelExecution,
     relativeUrlHeuristic,
@@ -6,16 +8,38 @@ import {
 import { createOrchestrator } from './index.js'
 import { createLocalFileDriver } from '@hookplane/statefile-driver'
 import path from 'path'
+import fs from 'fs/promises'
+import os from 'os'
 
 describe('orchestrator', () => {
+    let tmpDir: string
+    let statefilePath: string
+
+    beforeAll(async () => {
+        tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'orchestrator-test-'))
+        statefilePath = path.join(tmpDir, Date.now().toString() + '.statefile')
+        fs.writeFile(statefilePath, '')
+    })
+
+    afterAll(async () => {
+        await fs.rm(tmpDir, { recursive: true, force: true })
+    })
+
     it('runs', async () => {
+        const statefileDriver = createLocalFileDriver({ path: statefilePath })
+        const statefile = bootstrap('https://example.com' as BaseUrl)
+        const result = await statefileDriver.write(statefile)
+        if (result.isErr()) {
+            throw result.error
+        }
+
         const orchestrator = createOrchestrator({
             tsconfigPath: path.resolve(__dirname, '../test-proj/tsconfig.json'),
             execute: parallelExecution(),
             dispatch: defaultDispatch(),
-            statefileDriver: createLocalFileDriver({ path: 'TODO' }),
+            statefileDriver,
             matchingHeuristic: relativeUrlHeuristic,
         })
-        console.log(await orchestrator.run({ until: 'scanned' }))
+        console.log(await orchestrator.run({ until: 'statefile-parsed' }))
     })
 })
