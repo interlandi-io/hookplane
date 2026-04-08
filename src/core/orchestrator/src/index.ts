6d4bbf03e410897f8ed8e4d0b8f1ebd38ff9ef36
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-07 21:26:11 -0400

    better sketch

--- src/core/orchestrator/src/index.ts
+++ src/core/orchestrator/src/index.ts
@@ -9,89 +9,325 @@ import {
     createExecutor,
     StateUnknown,
     ProviderSet,
-    Statefile,
     State,
     Plan,
     Executor,
     StepId,
     StepState,
+    StatefileError,
+    SyncError,
+    PlanError,
+    MatchError,
+    ExecutorError,
 } from '@hookplane/core'
-import { StatefileDriver, StatefileDriverData } from '@hookplane/statefile-driver'
-import { findHookplane } from './find-hookplane.js'
-import { extract } from './extract.js'
+import {
+    StatefileDriver,
+    StatefileDriverData,
+    StatefileDriverError,
+} from '@hookplane/statefile-driver'
+import { FindError, findHookplane } from './find-hookplane.js'
+import { extract, ExtractionError } from './extract.js'
 
 export type Orchestrator = {
     run(): void
-    checkpoint(): OrchestratorState,
+    checkpoint(): OrchestratorState
 }
 
 export type OrchestratorDescriptor = {
     execute: ExecuteFn
     dispatch: DispatchFn
     statefileDriver: StatefileDriver
-    matchingHeuristic: Heuristic,
+    matchingHeuristic: Heuristic
 }
 
 export type OrchestratorState =
     | { tag: 'ready' }
-    | { tag: 'scanned'; stateUnknown: StateUnknown<ProviderSet> }
-    | { tag: 'statefile-loaded'; statefileData: StatefileDriverData }
-    | { tag: 'statefile-parsed'; statefile: Statefile<ProviderSet> }
-    | { tag: 'synced'; local: State<ProviderSet>, synced: State<ProviderSet>, diff: Plan<ProviderSet> }
+    | { tag: 'scanned'; rightUnknown: StateUnknown<ProviderSet> }
+    | {
+        tag: 'statefile-loaded'
+        rightUnknown: StateUnknown<ProviderSet>
+        leftStatefileData: StatefileDriverData
+    }
+    | {
+        tag: 'statefile-parsed'
+        leftPrior: State<ProviderSet>
+        rightUnknown: StateUnknown<ProviderSet>
+        // We could extract signing secrets here,
+        // but that's for future releases.
+    }
+    | {
+        tag: 'synced'
+        leftPrior: State<ProviderSet>
+        leftActual: State<ProviderSet>
+        rightUnknown: StateUnknown<ProviderSet>
+    }
+    | {
+        tag: 'drift-detected'
+        leftActual: State<ProviderSet>
+        plan: Plan<ProviderSet>
+        rightUnknown: StateUnknown<ProviderSet>
+    }
+    | {
+        tag: 'reconciled'
+        left: State<ProviderSet>
+        rightUnknown: StateUnknown<ProviderSet>
+    }
     | { tag: 'matched'; left: State<ProviderSet>; right: State<ProviderSet> }
     | { tag: 'planned'; plan: Plan<ProviderSet> }
-    | { tag: 'executing'; executor: Executor<ProviderSet> }
-    | { tag: 'success'; stepStates: Map<StepId, StepState> }
-    | { tag: 'failed'; error: Error; lastValidState: OrchestratorState }
+    | { tag: 'executable'; executor: Executor<ProviderSet> }
+    | { tag: 'succeeded'; stepStates: Map<StepId, StepState> }
+    | {
+        tag: 'failed'
+        error: OrchestratorError
+        lastValidState: OrchestratorState
+    }
 
-export function createOrchestrator() { }
+export type OrchestratorError =
+    | { last: 'ready'; error: FindError | ExtractionError }
+    | { last: 'scanned'; error: StatefileDriverError }
+    | { last: 'statefile-loaded'; error: StatefileError }
+    | { last: 'statefile-parsed'; error: SyncError }
+    | { last: 'synced'; error: PlanError }
+    | { last: 'drift-detected'; error: void } // TODO
+    | { last: 'reconciled'; error: MatchError }
+    | { last: 'matched'; error: PlanError }
+    | { last: 'planned'; error: ExecutorError }
+    | { last: 'executable'; error: Map<StepId, StepState> }
+
+export function createOrchestrator(desc: OrchestratorDescriptor) {
+    let state: OrchestratorState = { tag: 'ready' }
+
+    return {
+        run() { },
+    }
+}
 
 // Just a sketch here
 // eslint-disable-next-line
-async function run({
-    execute,
-    dispatch,
-    statefileDriver,
-    matchingHeuristic,
-}: OrchestratorDescriptor) {
-    // Before -- State: ready 
+async function transition(
+    state: OrchestratorState,
+    {
+        execute,
+        dispatch,
+        statefileDriver,
+        matchingHeuristic,
+    }: OrchestratorDescriptor,
+): Promise<OrchestratorState> {
+    switch (state.tag) {
+        case 'ready': {
+            const instance = findHookplane('TODO')
+            if (instance.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'ready',
+                        error: instance.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            const { exportName, filePath } = instance.value
+            const rightUnknown = await extract(exportName, filePath)
+            if (rightUnknown.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'ready',
+                        error: rightUnknown.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return { tag: 'scanned', rightUnknown: rightUnknown.value }
+        }
 
-    // State: Scanning 
-    const { filePath, exportName } = findHookplane('TODO')._unsafeUnwrap()
-    const stateUnknown = (await extract(exportName, filePath))._unsafeUnwrap()
+        case 'scanned': {
+            const { rightUnknown } = state
+            const leftStatefileData = await statefileDriver.read()
+            if (leftStatefileData.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'scanned',
+                        error: leftStatefileData.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'statefile-loaded',
+                rightUnknown,
+                leftStatefileData: leftStatefileData.value,
+            }
+        }
 
-    // State: Statefile retreival
-    const statefileData = statefileDriver.read()
+        case 'statefile-loaded': {
+            const { leftStatefileData, rightUnknown } = state
+            const leftStatefile = parseStatefile(
+                leftStatefileData,
+                rightUnknown.providers,
+            )
+            if (leftStatefile.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'statefile-loaded',
+                        error: leftStatefile.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            const leftPrior = leftStatefile.value.toState()
+            if (leftPrior.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'statefile-loaded',
+                        error: leftPrior.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'statefile-parsed',
+                leftPrior: leftPrior.value,
+                rightUnknown,
+            }
+        }
 
-    // State: statefile parsing
-    const statefile = parseStatefile(
-        statefileData,
-        stateUnknown.providers,
-    )._unsafeUnwrap()
+        case 'statefile-parsed': {
+            const { leftPrior, rightUnknown } = state
+            const leftActual = await sync(leftPrior.baseUrl, leftPrior.providers)
+            if (leftActual.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'statefile-parsed',
+                        error: leftActual.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'synced',
+                leftPrior,
+                leftActual: leftActual.value,
+                rightUnknown,
+            }
+        }
 
-    // State: Sync
-    const unsynced = statefile.toState()._unsafeUnwrap()
-    const synced = (
-        await sync(unsynced.baseUrl, unsynced.providers)
-    )._unsafeUnwrap()
-    const diff = createPlan(unsynced, synced)._unsafeUnwrap()
+        case 'synced': {
+            const { leftPrior, leftActual, rightUnknown } = state
+            const plan = createPlan(leftPrior, leftActual)
+            if (plan.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'synced',
+                        error: plan.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'drift-detected',
+                leftActual,
+                plan: plan.value,
+                rightUnknown,
+            }
+        }
 
-    // State: Sync reconciliation
-    if (!diff.isEmpty()) {
-        // reconciliation strategy
+        case 'drift-detected': {
+            const { leftActual, rightUnknown, } = state
+            // TODO: reconcile
+            return {
+                tag: 'reconciled',
+                left: leftActual,
+                rightUnknown,
+            }
+        }
+
+        case 'reconciled': {
+            const { left, rightUnknown } = state
+            const right = match(matchingHeuristic, rightUnknown, left)
+            if (right.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'reconciled',
+                        error: right.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'matched',
+                left,
+                right: right.value,
+            }
+        }
+
+        case 'matched': {
+            const { left, right } = state
+            const plan = createPlan(left, right)
+            if (plan.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'matched',
+                        error: plan.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'planned',
+                plan: plan.value,
+            }
+        }
+
+        case 'planned': {
+            const { plan } = state
+            const executor = createExecutor(plan, execute, dispatch)
+            if (executor.isErr()) {
+                return {
+                    tag: 'failed',
+                    error: {
+                        last: 'planned',
+                        error: executor.error,
+                    },
+                    lastValidState: state,
+                }
+            }
+            return {
+                tag: 'executable',
+                executor: executor.value,
+            }
+        }
+
+        case 'executable': {
+            const { executor } = state
+            await executor.execute()
+            const stepStates = executor.getStepStates()
+            const failed = stepStates.values().some(s => s.status !== 'success')
+            return !failed ? {
+                tag: 'succeeded',
+                stepStates,
+            } : {
+                tag: 'failed',
+                error: {
+                    last: 'executable',
+                    error: stepStates,
+                },
+                lastValidState: state,
+            }
+        }
+
+        // TODO: maybe error here?
+        case 'succeeded':
+            return state
+
+        case 'failed':
+            return state
     }
-    const left = synced
-
-    // State: Matching
-    const right = match(matchingHeuristic, stateUnknown, left)._unsafeUnwrap()
-
-    // State: Planning
-    const plan = createPlan(left, right)._unsafeUnwrap()
-
-    // State: execution
-    const executor = createExecutor(plan, execute, dispatch)._unsafeUnwrap()
-    await executor.execute()
-
-    // State: completed
-    const result = executor.getStepStates()
 }
