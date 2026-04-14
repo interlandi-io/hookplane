5110299804de294f71f700b65bc6d7f00e21ff69
Michael Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-14 13:27:31 -0400

    (feat) Basic CLI Sketch (#30)
    * remove cli package
    
    * add cli bin to hookplane
    
    * basic test
    
    * add shouldbootstrap to sketch

--- src/core/cli/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import { config } from "@repo/eslint-config/server-lib";
-
-/** @type {import("eslint").Linter.Config} */
-export default [
-    ...config,
-    {
-        ignores: ['vitest.config.mjs'],
-    },
-];
