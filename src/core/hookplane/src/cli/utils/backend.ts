08ff0a2a2adbaa15eb56ad29292b3d125ad3ccfa
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-18 16:09:26 -0400

    refactor cli

--- src/core/hookplane/src/cli/utils/backend.ts
+++ src/core/hookplane/src/cli/utils/backend.ts
@@ -1,33 +1,29 @@
 import { err, ok, Result } from 'neverthrow'
 import { Backend } from '@hookplane/backend'
-import { parseStatefile, ProviderSet, State, sync } from '@hookplane/core'
+import { ProviderSet, State, sync } from '@hookplane/core'
+import { logger } from '../logger.js'
 
 export async function getPrior<P extends ProviderSet>(
     backend: Backend,
     providers: P,
 ): Promise<Result<State<P>, Error>> {
-    const data = await backend.statefile.read()
-    if (data.isErr()) {
-        return err(new Error(data.error.message))
-    }
-    const statefile = parseStatefile(data.value, providers)
-    if (statefile.isErr()) {
-        return err(new Error(statefile.error.message))
-    }
-    const prior = statefile.value.toState()
+    logger.debug('getting prior state')
+    const prior = await backend.state.read(providers)
     if (prior.isErr()) {
         return err(new Error(prior.error.message))
     }
-
+    logger.dir('prior state', prior.value)
     return ok(prior.value)
 }
 
 export async function getActual<P extends ProviderSet>(
     providers: P,
 ): Promise<Result<State<P>, Error>> {
+    logger.debug('getting actual state')
     const actual = await sync(providers)
     if (actual.isErr()) {
         return err(new Error(actual.error.message))
     }
+    logger.dir('actual state', actual.value)
     return ok(actual.value)
 }
