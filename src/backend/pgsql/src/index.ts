7cc703e15f0b64e905ac440fb22c9b560a523f9d
Mike Interlandi <43190101+Minterl@users.noreply.github.com>
2026-04-20 13:52:55 -0400

    applyEvent

--- src/backend/pgsql/src/index.ts
+++ src/backend/pgsql/src/index.ts
@@ -1,8 +1,11 @@
-import { describeBackend } from '@hookplane/backend'
+import { BackendError, describeBackend, StateEvent, WriteRejectedError } from '@hookplane/backend'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { migrate } from 'drizzle-orm/node-postgres/migrator'
 import path from 'path'
 import * as schema from './db/schema.js'
+import { Provider } from '@hookplane/core'
+import { eq } from 'drizzle-orm'
+import { ResultAsync } from 'neverthrow'
 
 export type PgsqlBackendConfig = {
     databaseUrl: string
@@ -10,8 +13,10 @@ export type PgsqlBackendConfig = {
 }
 
 type PgsqlBackendState = {
-    db: ReturnType<typeof drizzle<typeof schema>>,
+    db: Database
 }
+
+type Database = ReturnType<typeof drizzle<typeof schema>>
 
 export const createPgsqlBackend = describeBackend<PgsqlBackendConfig, PgsqlBackendState>({
     name: 'pgsql',
@@ -28,6 +33,16 @@ export const createPgsqlBackend = describeBackend<PgsqlBackendConfig, PgsqlBacke
         read: () => { throw '' },
         write: () => { throw '' },
         delete: () => { throw '' },
+        events: {
+            apply({ state: { db }, events }) {
+                const results: ResultAsync<void, BackendError>[] = events
+                    .map(event => ResultAsync.fromPromise(
+                        applyEvent(db, event),
+                        (e) => e as BackendError // TODO
+                    ))
+                return ResultAsync.combine(results).map(() => {})
+            },
+        }
     },
     signingSecret: {
         read: () => { throw '' },
@@ -36,3 +51,75 @@ export const createPgsqlBackend = describeBackend<PgsqlBackendConfig, PgsqlBacke
     },
 })
 
+async function applyEvent(db: Database, event: StateEvent<Provider>) {
+    let provider = (
+        await db
+            .select({ id: schema.providers.id })
+            .from(schema.providers)
+            .where(eq(schema.providers.name, event.provider.name))
+            .limit(1)
+    )[0]
+
+    if (!provider) {
+        const inserted = (
+            await db
+                .insert(schema.providers)
+                .values({ name: event.provider.name })
+                .returning()
+        )[0]
+        if (!inserted) {
+            throw {
+                kind: 'BackendError',
+                name: 'WriteRejectedError',
+                message: `provider ${event.provider.name} could not be found or created`,
+                while: 'write'
+            } satisfies WriteRejectedError 
+        }
+        provider = inserted
+    }
+
+    switch (event.tag) {
+        case 'endpoint.created':
+            await db.insert(schema.endpoints).values({
+                providerId: provider.id,
+                handle: event.handle,
+                url: event.state.url,
+                events: event.state.events,
+                config: event.state.config,
+            })
+            break
+
+        case 'endpoint.updated':
+            await db
+                .update(schema.endpoints)
+                .set({
+                    url: event.after.url,
+                    handle: event.handle,
+                    events: event.after.events,
+                    config: event.after.config,
+                })
+                .where(eq(schema.endpoints.handle, event.handle))
+            break
+
+        case 'endpoint.deleted':
+            await db
+                .delete(schema.endpoints)
+                .where(eq(schema.endpoints.handle, event.handle))
+            // If there are no events under this provider,
+            // we should delete it.
+            const providerEvents = await db
+                .select()
+                .from(schema.endpoints)
+                .where(eq(schema.endpoints.providerId, provider.id))
+            if (providerEvents.length === 0) {
+                await db
+                    .delete(schema.providers)
+                    .where(eq(schema.providers.id, provider.id))
+            }
+            break
+    }
+}
+
+function toBackendError(e: unknown) {
+
+}
--- src/backend/pgsql/test/index.test.ts
+++ src/backend/pgsql/test/index.test.ts
@@ -1,38 +1,43 @@
 import { Client } from 'pg'
 import { createPgsqlBackend } from '~/index.js'
+import { Backend, StateEvent } from '@hookplane/backend'
+import { EndpointHandle, EndpointUrl, Provider } from '@hookplane/core'
 import * as schema from '../src/db/schema.js'
-import { Backend } from '@hookplane/backend'
 
 const databaseUrl = process.env['DATABASE_URL']
 const describeIntegration = databaseUrl ? describe : describe.skip
+
 const itWrapped = 
     (name: string, fn: (params: { backend: Backend, client: Client }) => Promise<void>) => {
         it(name, async () => {
-            const backend = await createPgsqlBackend({
-                databaseUrl: databaseUrl!,
-                runMigrations: true,
-            })()
-
             const client = new Client({ connectionString: databaseUrl })
             await client.connect()
 
             try {
+                const backend = await createPgsqlBackend({
+                    databaseUrl: databaseUrl!,
+                    runMigrations: true,
+                })()
+
                 await fn({ backend, client })
             } finally {
+                await client.query('delete from providers')
+                await client.query('delete from endpoints')
+                await client.query('delete from secrets')
+
                 await client.end()
             }
-                
         })
     }
 
 describeIntegration('pgsql backend', () => {
     itWrapped('initializes and runs migrations', async ({ backend, client }) => {
         expect(backend.name).toBe('pgsql')
-        const tables = await client.query<typeof schema>(`
-            SELECT
-                to_regclass('public.providers') AS providers,
-                to_regclass('public.endpoints') AS endpoints,
-                to_regclass('public.secrets') AS secrets
+        const tables = await client.query<{ providers: string, endpoints: string, secrets: string }>(`
+            select
+                to_regclass('public.providers') as providers,
+                to_regclass('public.endpoints') as endpoints,
+                to_regclass('public.secrets') as secrets
         `)
 
         expect(tables.rows[0]).toEqual({
@@ -40,5 +45,72 @@ describeIntegration('pgsql backend', () => {
             endpoints: 'endpoints',
             secrets: 'secrets',
         })
+    })
+
+    itWrapped('applies events', async ({ backend, client }) => {
+        const providersBefore = await client.query('select * from providers')
+        const endpointsBefore = await client.query('select * from endpoints')
+        expect(providersBefore.rowCount).toBe(0)
+        expect(endpointsBefore.rowCount).toBe(0)
+
+        const mockProvider = { name: 'mock' } as Provider
+        const create: StateEvent<typeof mockProvider> = {
+            tag: 'endpoint.created',
+            provider: mockProvider,
+            handle: 'handle-0' as EndpointHandle,
+            state: {
+                url: 'https://example.com/hooks/mock' as EndpointUrl,
+                events: ['event'],
+                config: {} 
+            },
+        }
+        const update: StateEvent<typeof mockProvider> = {
+            tag: 'endpoint.updated',
+            provider: mockProvider,
+            handle: 'handle-0' as EndpointHandle,
+            before: {
+                url: 'https://example.com/hooks/mock' as EndpointUrl,
+                events: ['event'],
+                config: {} 
+            },
+            after: {
+                url: 'https://example.com/hooks/mock' as EndpointUrl,
+                events: ['event', 'event2'],
+                config: {} 
+            },
+        }
+        const _delete: StateEvent<typeof mockProvider> = {
+            tag: 'endpoint.deleted',
+            provider: mockProvider,
+            handle: 'handle-0' as EndpointHandle,
+        }
+
+        let result = await backend.state.events.apply([create])
+        expect(result.isOk()).toBe(true)
+        const providersAfterCreate = await client.query<typeof schema['providers']>('select * from providers')
+        const endpointsAfterCreate = await client.query<typeof schema['endpoints']>('select * from endpoints')
+        expect(providersAfterCreate.rowCount).toBe(1)
+        expect(providersAfterCreate.rows[0]?.name).toBe('mock')
+        expect(endpointsAfterCreate.rowCount).toBe(1)
+        expect(endpointsAfterCreate.rows[0]?.handle).toBe('handle-0')
+
+        result = await backend.state.events.apply([update])
+        expect(result.isOk()).toBe(true)
+        const providersAfterUpdate = await client.query<typeof schema['providers']>('select * from providers')
+        const endpointsAfterUpdate = await client.query<typeof schema['endpoints']>('select * from endpoints')
+        expect(providersAfterUpdate.rowCount).toBe(1)
+        expect(providersAfterUpdate.rows[0]?.name).toBe('mock')
+        expect(endpointsAfterUpdate.rowCount).toBe(1)
+        expect(endpointsAfterUpdate.rows[0]?.handle).toBe('handle-0')
+        expect(endpointsAfterUpdate.rows[0]?.events).toContainEqual('event')
+        expect(endpointsAfterUpdate.rows[0]?.events).toContainEqual('event2')
+
+
+        result = await backend.state.events.apply([_delete])
+        expect(result.isOk()).toBe(true)
+        const providersAfterDelete = await client.query<typeof schema['providers']>('select * from providers')
+        const endpointsAfterDelete = await client.query<typeof schema['endpoints']>('select * from endpoints')
+        expect(providersAfterDelete.rowCount).toBe(0)
+        expect(endpointsAfterDelete.rowCount).toBe(0)
     })
 })
